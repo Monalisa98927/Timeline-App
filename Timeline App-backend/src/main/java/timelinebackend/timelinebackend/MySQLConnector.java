c454843cccae965969d884d08889292bffe160f4
Monalisa98927 <380923800@qq.com>
2019-01-01 03:12:46 +0800

    4th commit

--- Timeline App-backend/src/main/java/timelinebackend/timelinebackend/MySQLConnector.java
+++ Timeline App-backend/src/main/java/timelinebackend/timelinebackend/MySQLConnector.java
@@ -4,6 +4,7 @@ import java.sql.*;
 
 public class MySQLConnector {
 
+    public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     public static final String DB_URL = "jdbc:mysql://localhost:3306/timeline?useSSL=false&serverTimezone=GMT%2B8";
     public static final String USER = "root";
     public static final String PASS = "123456";
